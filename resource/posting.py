5aed2af1b5270a64ced18824e042b96836caed34
ghdrl858 <ghdrl858@gmail.com>
2022-06-30 10:33:50 +0900

    2022-06-30

--- resource/posting.py
+++ resource/posting.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from http import HTTPStatus
-import re
 from flask import request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_restful import Resource
